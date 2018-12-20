5aa3b33dbf68c8c3c568b2375cfd759bc89b104a
Murilo Calegari <murilo.calegari.souza@gmail.com>
2018-12-20 20:01:14 -0200

    Set first activity as checked in the navigation drawer

--- app/src/main/java/calegari/murilo/agendaescolar/MainActivity.java
+++ app/src/main/java/calegari/murilo/agendaescolar/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.getMenu().getItem(0).setChecked(true); // Defines this activity as checked
     }
 
     @Override
