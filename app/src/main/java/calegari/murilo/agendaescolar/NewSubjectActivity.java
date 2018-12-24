569633de70decf7664f20ef23c13bb0a0d837c7d
Murilo Calegari <murilo.calegari.souza@gmail.com>
2018-12-24 17:59:51 -0200

    Initial subjects layout

--- app/src/main/java/calegari/murilo/agendaescolar/NewSubjectActivity.java
+++ app/src/main/java/calegari/murilo/agendaescolar/NewSubjectActivity.java
@@ -1,32 +1,33 @@
 package calegari.murilo.agendaescolar;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
-public class SettingsActivity extends AppCompatActivity {
+public class NewSubjectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_new_subject);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Finishes the Activity when the toolbar back button is pressed
+        // Finishes the Activity
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-
-        // Instantiates the settings fragment
-        getFragmentManager().beginTransaction().replace(R.id.settings_frame_layout, new SettingsFragment(), "preferences").commit();
     }
+
+
+
 }
-
-
--- app/src/main/java/calegari/murilo/agendaescolar/SubjectsFragment.java
+++ app/src/main/java/calegari/murilo/agendaescolar/SubjectsFragment.java
@@ -1,13 +1,18 @@
 package calegari.murilo.agendaescolar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class SubjectsFragment extends Fragment {
+
+    FloatingActionButton fab;
 
     @Nullable
     @Override
@@ -16,4 +21,23 @@ public class SubjectsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_subjects, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fab = getView().findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newSubject();
+            }
+        });
+
+    }
+
+    private void newSubject() {
+        Intent newSubjectIntent = new Intent(getContext(), NewSubjectActivity.class);
+        startActivity(newSubjectIntent);
+    }
 }
