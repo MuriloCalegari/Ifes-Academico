cdd8bcd3676d95379dfe0fca322398b22d8fedc7
MuriloCalegari <murilo.calegari.souza@gmail.com>
2018-12-28 13:19:41 -0200

    Added delete subject button

--- app/src/main/java/calegari/murilo/agendaescolar/subjecthelper/EditSubjectActivity.java
+++ app/src/main/java/calegari/murilo/agendaescolar/subjecthelper/EditSubjectActivity.java
@@ -1,8 +1,10 @@
 package calegari.murilo.agendaescolar.subjecthelper;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ImageButton;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import calegari.murilo.agendaescolar.databases.SubjectDatabaseHelper;
@@ -15,85 +17,119 @@ import ernestoyaquello.com.verticalstepperform.listener.StepperFormListener;
 
 public class EditSubjectActivity extends AppCompatActivity implements StepperFormListener {
 
-    private SubjectNameStep newSubjectName;
-    private SubjectAbbreviationStep newSubjectAbbreviation;
-    private SubjectProfessorStep newSubjectProfessor;
-    private VerticalStepperFormView verticalStepperForm;
+	private SubjectNameStep newSubjectName;
+	private SubjectAbbreviationStep newSubjectAbbreviation;
+	private SubjectProfessorStep newSubjectProfessor;
+	private VerticalStepperFormView verticalStepperForm;
 
-    String subjectName;
-    String subjectProfessor;
-    String subjectAbbreviation;
+	String subjectName;
+	String subjectProfessor;
+	String subjectAbbreviation;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_subject);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_subject);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Finishes the Activity
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+		// Finishes the Activity
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 
         /*
         Used library: VerticalStepperForm, available at:
         https://github.com/ernestoyaquello/VerticalStepperForm
         */
 
-        // Gets information from which input the user would like to edit
-        subjectName = getIntent().getStringExtra("subjectName");
-        subjectProfessor = getIntent().getStringExtra("subjectProfessor");
-        subjectAbbreviation = getIntent().getStringExtra("subjectAbbreviation");
+		// Gets information from which input the user would like to edit
+		subjectName = getIntent().getStringExtra("subjectName");
+		subjectProfessor = getIntent().getStringExtra("subjectProfessor");
+		subjectAbbreviation = getIntent().getStringExtra("subjectAbbreviation");
 
-        // Create the steps
-        newSubjectName = new SubjectNameStep(getResources().getString(R.string.name));
-        newSubjectAbbreviation = new SubjectAbbreviationStep(getResources().getString(R.string.abbreviation), true, subjectAbbreviation);
-        newSubjectProfessor = new SubjectProfessorStep(getResources().getString(R.string.professor));
+		// Create the steps
+		newSubjectName = new SubjectNameStep(getResources().getString(R.string.name));
+		newSubjectAbbreviation = new SubjectAbbreviationStep(getResources().getString(R.string.abbreviation), true, subjectAbbreviation);
+		newSubjectProfessor = new SubjectProfessorStep(getResources().getString(R.string.professor));
 
-        // Find the form view, set it up and initialize it.
-        verticalStepperForm = findViewById(R.id.stepper_form);
-        verticalStepperForm
-                .setup(this, newSubjectName, newSubjectAbbreviation, newSubjectProfessor)
-                .lastStepNextButtonText(getString(R.string.subject_confirm_save_button))
-                .displayCancelButtonInLastStep(true)
-                .lastStepCancelButtonText(getString(R.string.cancel))
-                .stepNextButtonText(getString(R.string.next))
-                .init();
+		// Find the form view, set it up and initialize it.
+		verticalStepperForm = findViewById(R.id.stepper_form);
+		verticalStepperForm
+				.setup(this, newSubjectName, newSubjectAbbreviation, newSubjectProfessor)
+				.lastStepNextButtonText(getString(R.string.subject_confirm_save_button))
+				.displayCancelButtonInLastStep(true)
+				.lastStepCancelButtonText(getString(R.string.cancel))
+				.stepNextButtonText(getString(R.string.next))
+				.init();
 
-        reloadSteps(subjectName, subjectProfessor, subjectAbbreviation);
-    }
+		reloadSteps(subjectName, subjectProfessor, subjectAbbreviation);
 
-    @Override
-    public void onCompletedForm() {
-        // This method will be called when the user clicks on the last confirmation button of the
-        // form in an attempt to save or send the data.
+		ImageButton imageButton = findViewById(R.id.imageButton);
 
-        // Sends data to database
-        SubjectDatabaseHelper subjectDbHelper = new SubjectDatabaseHelper(this);
-        Integer result = subjectDbHelper.removeData(subjectAbbreviation); // Deletes entry from database so it will be regenerated
-        subjectDbHelper.insertData(
-                newSubjectName.getStepDataAsHumanReadableString(),
-                newSubjectAbbreviation.getStepDataAsHumanReadableString(),
-                newSubjectProfessor.getStepDataAsHumanReadableString());
+		imageButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				deleteSubject(subjectAbbreviation);
+			}
+		});
 
-        finish();
-    }
+	}
 
-    @Override
-    public void onCancelledForm() {
-        // This method will be called when the user clicks on the cancel button of the form.
-        finish();
-    }
+	@Override
+	public void onCompletedForm() {
+		// This method will be called when the user clicks on the last confirmation button of the
+		// form in an attempt to save or send the data.
 
-    public void reloadSteps(String subjectName, String subjectProfessor, String subjectAbbreviation) {
-        newSubjectName.restoreStepData(subjectName);
-        newSubjectProfessor.restoreStepData(subjectProfessor);
-        newSubjectAbbreviation.restoreStepData(subjectAbbreviation);
-    }
+		// Sends data to database
+		SubjectDatabaseHelper subjectDbHelper = new SubjectDatabaseHelper(this);
+		subjectDbHelper.removeData(subjectAbbreviation); // Deletes entry from database so it will be regenerated
+		subjectDbHelper.insertData(
+				newSubjectName.getStepDataAsHumanReadableString(),
+				newSubjectAbbreviation.getStepDataAsHumanReadableString(),
+				newSubjectProfessor.getStepDataAsHumanReadableString());
+
+		finish();
+	}
+
+	@Override
+	public void onCancelledForm() {
+		// This method will be called when the user clicks on the cancel button of the form.
+		finish();
+	}
+
+	public void reloadSteps(String subjectName, String subjectProfessor, String subjectAbbreviation) {
+		newSubjectName.restoreStepData(subjectName);
+		newSubjectProfessor.restoreStepData(subjectProfessor);
+		newSubjectAbbreviation.restoreStepData(subjectAbbreviation);
+	}
+
+	public void deleteSubject(final String subjectAbbreviation) {
+		final SubjectDatabaseHelper subjectDbHelper = new SubjectDatabaseHelper(this);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder
+				.setTitle(getString(R.string.confirm_subject_delete_title))
+				.setMessage(getString(R.string.confirm_subject_delete_message))
+				.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						subjectDbHelper.removeData(subjectAbbreviation);
+						// TODO: Remove all entries in grades database
+						finish();
+					}
+				})
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+
+					}
+				})
+				.show();
+	}
 }
