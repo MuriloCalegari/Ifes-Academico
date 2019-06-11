ea107bc05869c095b21e07d52f3ac3582b862ea2
Murilo Calegari <murilo.calegari.souza@gmail.com>
2019-06-10 22:23:29 -0300

    Only enable analytics if user allows

--- app/src/main/java/calegari/murilo/sistema_academico/MyApplication.java
+++ app/src/main/java/calegari/murilo/sistema_academico/MyApplication.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import calegari.murilo.sistema_academico.utils.Constants;
 import io.fabric.sdk.android.Fabric;
@@ -16,6 +17,7 @@ public class MyApplication extends Application {
 
 		if(PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.Keys.IS_DATA_COLLECTION_AUTHORIZED, false)) {
 			Fabric.with(this, new Crashlytics());
+			FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(true);
 		}
 		//Stetho.initializeWithDefaults(this);
 	}
